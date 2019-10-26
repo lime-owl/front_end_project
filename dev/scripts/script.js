743cafd8530a27b64494a8e2a43fe56e12398240
Tai Do <end_lesslove2012@yahoo.com>
2019-10-25 23:16:35 -0700

    Navigation bar

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,11 @@
 // Write your JavaScript here...
+// Navigation
+$(document).ready(() => {
+  $('#toggle-menu').click(function () {
+    if ($(this).prop('checked')) {
+      $('header nav').slideDown();
+    } else {
+      $('header nav').slideUp();
+    }
+  });
+});
