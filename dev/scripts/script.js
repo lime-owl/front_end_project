0949144e2d7bb39cbbc513f82b63971bd031c048
Tai Do <end_lesslove2012@yahoo.com>
2019-10-26 15:58:07 -0700

    Our story

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,11 +1,21 @@
 // Write your JavaScript here...
-// Navigation
+
 $(document).ready(() => {
+  // Navigation
   $('#toggle-menu').click(function () {
     if ($(this).prop('checked')) {
       $('header nav').slideDown();
     } else {
       $('header nav').slideUp();
     }
+  });
+
+  // Landing carousel
+  $('.landing-carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
   });
 });
